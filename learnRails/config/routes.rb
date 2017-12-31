ce7e5c847a18a4b58801ca5e20b760bade64ec6d
Tabitha Levine <tabithalevine@hotmail.com>
2017-12-31 09:33:54 -0500

    3.8 Add Home, About and Contact pages

--- learnRails/config/routes.rb
+++ learnRails/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'simple_pages/about'
+
+  get 'simple_pages/contact'
+
+  get 'simple_pages/index'
+  root 'simple_pages#index'
   # set the index page / root url
   root 'pages#home'
   get 'pages/about'
