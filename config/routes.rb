e70ebe069ff9158b79093fc65558a999c2d4c7e4
Tabitha4 <tabithalevine@hotmail.co>
2018-01-19 08:47:31 -0500

    4.1 add scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,15 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'simple_pages/about'
-
   get 'simple_pages/contact'
-
-  get 'simple_pages/products'
-
   get 'simple_pages/index'
-  root 'simple_pages#index'
-  # set the index page / root url
+  # get 'simple_pages/products'
+
+  root 'simple_pages#index' # set the index page / root url
   root 'simple_pages#home'
-  get 'simple_pages/about'
 
-  get 'simple_pages/home'
-
-  get 'simple_pages/contact'
-
-  get 'simple_pages/products'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
