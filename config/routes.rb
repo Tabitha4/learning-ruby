3258a48457b41554baf6687b022272add95febcf
Tabitha4 <tabithalevine@hotmail.co>
2018-02-02 10:27:02 -0500

    4.3 add landing_page and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  # get 'simple_pages/products'
+  get 'simple_pages/landing_page'
 
-  root 'simple_pages#index' # set the index page / root url
-
-
+  root 'simple_pages#landing_page' # set the landing page / root url
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
