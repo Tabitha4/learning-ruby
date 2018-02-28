7a9197662e9bb96db310d0cf2eb8f0952466e864
Tabitha4 <tabithalevine@hotmail.co>
2018-02-28 15:45:03 -0500

    4.6 add changes to order controller and the order.html

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
-
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  
   def index
     @orders = Order.includes(:product).all
   end
