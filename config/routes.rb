ade96e7ce5c1d7d147ef0073d1d51591ebc4f99a
Tabitha4 <tabithalevine@hotmail.co>
2018-04-10 20:10:26 -0400

    fix userIndex and responsiveness

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  
+
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
-  #get '/products', to: 'products#index'
+  
 
   post 'simple_pages/thank_you'
 
